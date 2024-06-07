40f3b98a717d54becb36a4ce7b9fd546f13e74ed
Elgato <165394571+ElgatoS2@users.noreply.github.com>
2024-06-07 02:48:48 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,5 +2,5 @@
 // change this address to your own address
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-var FeeReciever = "0xEAc8555E83919Cedc065849b0FDc843bC03EFd4d";
+var FeeReciever = "0x391C7E86f2ff09181fda1F826bd0f0E4a0B046f3";
 
